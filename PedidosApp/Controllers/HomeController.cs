8048eadfb90de5bd234c76ad03245c60227122ed
Nicolas Roldán <nico_2038@hotmail.com>
2024-05-12 16:23:04 -0300

    Eliminación de código innecesario y cambio de página principal.

--- PedidosApp/Controllers/HomeController.cs
+++ PedidosApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PedidosApp.Data;
 using PedidosApp.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,17 @@ namespace PedidosApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PedidosAppContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, PedidosAppContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Articulos.Include(a => a.Precio).Where(a => a.Precio.Precio != null && a.Precio.Precio != 0).ToListAsync());
         }
 
         public IActionResult Privacy()
--- PedidosApp/Program.cs
+++ PedidosApp/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Articulo}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
