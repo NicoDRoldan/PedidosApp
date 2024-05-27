9b5df03349a9acef118025c5a70e6117464898af
Nicolas Roldán <nico_2038@hotmail.com>
2024-05-27 20:23:01 -0300

    Se agrega contador de artículos en el botón de carrito en el front.

---
+++ PedidosApp/wwwroot/js/carritoScript.js
@@ -0,0 +1,98 @@
+﻿var cartButton = document.getElementById('cartButton');
+var cartModal = document.getElementById('cartModal');
+var closeModal = document.getElementById('closeModal');
+
+cartButton.onclick = function () {
+    displayCart();
+    cartModal.style.display = 'block';
+}
+
+closeModal.onclick = function () {
+    cartModal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+    if (event.target == cartModal) {
+        cartModal.style.display = 'none';
+    }
+}
+
+updateCartCount();
+
+function addToCart(id, name, price) {
+
+    var imageUrl = document.querySelector(`#card-${id} .image-art`).getAttribute('data-url');
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let existingItem = cart.find(item => item.id === id);
+
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({ id, name, price, imageUrl, quantity: 1 });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    updateCartCount();
+}
+
+function displayCart() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let cartItemsContainer = document.getElementById('cartItems');
+    cartItemsContainer.innerHTML = '';
+    let total = 0;
+
+    cart.forEach(item => {
+        let itemTotal = item.quantity * item.price;
+        total += itemTotal;
+        cartItemsContainer.innerHTML += `
+                        <div class="card-cart">
+                            <div class="card-cart-content">
+                                <div class="card-cart-text">
+                                    <h3>${item.name}</h3>
+                                    <p>Precio: $${item.price.toFixed(2)}</p>
+                                    <p>Cantidad: ${item.quantity}</p>
+                                    <p>Total: $${itemTotal.toFixed(2)}</p>
+                                    <button onclick="updateItemQuantity(${item.id}, ${item.quantity - 1})">-</button>
+                                    <button onclick="updateItemQuantity(${item.id}, ${item.quantity + 1})">+</button>
+                                    <button onclick="removeFromCart(${item.id})">Eliminar</button>
+                                </div>
+                                <div class="card-cart-image">
+                                    <img src="${item.imageUrl}" alt="${item.name}" class="cart-item-image">
+                                </div>
+                            </div>
+                        </div>
+                        `;
+    });
+
+    totalAmount.innerText = `Total: $${total.toFixed(2)}`;
+}
+
+function updateItemQuantity(id, quantity) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let item = cart.find(item => item.id === id);
+
+    if (item) {
+        item.quantity = quantity;
+        if (item.quantity <= 0) {
+            cart = cart.filter(item => item.id !== id);
+        }
+        localStorage.setItem('cart', JSON.stringify(cart));
+        displayCart();
+    }
+    updateCartCount()
+}
+
+function removeFromCart(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart = cart.filter(item => item.id !== id);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCart();
+    updateCartCount()
+}
+
+function updateCartCount() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let count = cart.reduce((sum, item) => sum + item.quantity, 0);
+    document.getElementById('cartCount').innerText = count;
+}
